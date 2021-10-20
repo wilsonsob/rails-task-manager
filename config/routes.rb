d70c1e1bf208a15efaa7a5e4f6f6780f5747c852
Wilson Filho <wilsonsenhor@gmail.com>
2021-10-20 16:52:42 -0300

    Added controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'tasks#index' # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #create
+  get 'task/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+  #show
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  #destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  root to: 'tasks#index'
 end
